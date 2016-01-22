dc17eb4e55c9d754fa8d8a4d10e42017313e1267
jacobemerick <jpemeric@gmail.com>
2016-01-22 04:55:33 -0700

    Adds tests to cover recipient settings

--- tests/ArchangelTest.php
+++ tests/ArchangelTest.php
@@ -3,6 +3,7 @@
 namespace Jacobemerick\Archangel;
 
 use PHPUnit_Framework_TestCase;
+use ReflectionClass;
 
 class ArchangelTest extends PHPUnit_Framework_TestCase
 {
@@ -78,5 +79,95 @@ class ArchangelTest extends PHPUnit_Framework_TestCase
         $archangel->addTo('test@example.com', 'Mr. Test Alot');
 
         $this->assertAttributeContains('"Mr. Test Alot" <test@example.com>', 'toAddresses', $archangel);
+    }
+
+    public function testAddCc()
+    {
+        $archangel = new Archangel();
+        $archangel->addCc('test@example.com');
+
+        $this->assertAttributeContains('test@example.com', 'ccAddresses', $archangel);
+    }
+
+    public function testAddCcMultiple()
+    {
+        $archangel = new Archangel();
+        $archangel->addCc('testOne@example.com');
+        $archangel->addCc('testTwo@example.com');
+
+        $this->assertAttributeContains('testOne@example.com', 'ccAddresses', $archangel);
+        $this->assertAttributeContains('testTwo@example.com', 'ccAddresses', $archangel);
+    }
+
+    public function testAddCcWithTitle()
+    {
+        $archangel = new Archangel();
+        $archangel->addCc('test@example.com', 'Mr. Test Alot');
+
+        $this->assertAttributeContains('"Mr. Test Alot" <test@example.com>', 'ccAddresses', $archangel);
+    }
+
+    public function testAddBcc()
+    {
+        $archangel = new Archangel();
+        $archangel->addBcc('test@example.com');
+
+        $this->assertAttributeContains('test@example.com', 'bccAddresses', $archangel);
+    }
+
+    public function testAddBccMultiple()
+    {
+        $archangel = new Archangel();
+        $archangel->addBcc('testOne@example.com');
+        $archangel->addBcc('testTwo@example.com');
+
+        $this->assertAttributeContains('testOne@example.com', 'bccAddresses', $archangel);
+        $this->assertAttributeContains('testTwo@example.com', 'bccAddresses', $archangel);
+    }
+
+    public function testAddBccWithTitle()
+    {
+        $archangel = new Archangel();
+        $archangel->addBcc('test@example.com', 'Mr. Test Alot');
+
+        $this->assertAttributeContains('"Mr. Test Alot" <test@example.com>', 'bccAddresses', $archangel);
+    }
+
+    public function testSetFrom()
+    {
+        $archangel = new Archangel();
+        $archangel->setFrom('test@example.com');
+        $setHeaders = $this->getProtectedProperty($archangel, 'headers');
+
+        $this->assertArraySubset(array('From' => 'test@example.com'), $setHeaders);
+    }
+
+    public function testSetFromMultiple()
+    {
+        $archangel = new Archangel();
+        $archangel->setFrom('testOne@example.com');
+        $archangel->setFrom('testTwo@example.com');
+        $setHeaders = $this->getProtectedProperty($archangel, 'headers');
+
+        $this->assertArraySubset(array('From' => 'testTwo@example.com'), $setHeaders);
+        $this->assertNotContains('testOne@example.com', $setHeaders);
+    }
+
+    public function testSetFromWithTitle()
+    {
+        $archangel = new Archangel();
+        $archangel->setFrom('test@example.com', 'Mr. Test Alot');
+        $setHeaders = $this->getProtectedProperty($archangel, 'headers');
+
+        $this->assertArraySubset(array('From' => '"Mr. Test Alot" <test@example.com>'), $setHeaders);
+    }
+
+    protected function getProtectedProperty($archangel, $property)
+    {
+        $reflectedArchangel = new ReflectionClass($archangel);
+        $reflectedProperty = $reflectedArchangel->getProperty($property);
+        $reflectedProperty->setAccessible(true);
+
+        return $reflectedProperty->getValue($archangel);
     }
 }
